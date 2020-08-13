5f2c741f337c81b4c66b2d15fe4ebe1444bc6fa8
Sergey Shushert <sergeyshushert@outlook.com>
2020-08-13 21:06:54 +0300

    Added prj ParsingReadfootball.

---
+++ ParsingReadfootball/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingReadfootball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<List<string>>> result = Parsing(url: "https://www.readfootball.com/tables.html");
+            if(result != null)
+            {
+                foreach(var item in result)
+                {
+                    Console.WriteLine("-----------------------------------------");
+                    Console.WriteLine(item.Key);
+                    Console.WriteLine("-----------------------------------------");
+                    item.Value.ForEach(r => Console.WriteLine(string.Join("\t", r)));
+                    Console.WriteLine("-----------------------------------------\n");
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        private static Dictionary<string, List<List<string>>> Parsing(string url)
+        {
+            try
+            {
+                Dictionary<string, List<List<string>>> result = new Dictionary<string, List<List<string>>>();
+
+                using (HttpClientHandler hdl = new HttpClientHandler { AllowAutoRedirect = false, AutomaticDecompression = System.Net.DecompressionMethods.Deflate | System.Net.DecompressionMethods.GZip | System.Net.DecompressionMethods.None })
+                {
+                    using (var clnt = new HttpClient(hdl))
+                    {
+                        using (HttpResponseMessage resp = clnt.GetAsync(url).Result)
+                        {
+                            if (resp.IsSuccessStatusCode)
+                            {
+                                var html = resp.Content.ReadAsStringAsync().Result;
+                                if (!string.IsNullOrEmpty(html))
+                                {
+                                    HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+                                    doc.LoadHtml(html);
+
+                                    var tables = doc.DocumentNode.SelectNodes(".//div[@class='block_content']//div[@class='two-table-row']//div[@class]");
+                                    if (tables != null && tables.Count > 0)
+                                    {
+                                        foreach (var table in tables)
+                                        {
+                                            var titleNode = table.SelectSingleNode(".//div[@class='head_tb']");
+                                            if (titleNode != null)
+                                            {
+                                                var tbl = table.SelectSingleNode(".//div[@class='tab_champ']//table");
+                                                if(tbl != null)
+                                                {
+                                                    var rows = tbl.SelectNodes(".//tr");
+                                                    if(rows != null && rows.Count > 0)
+                                                    {
+                                                        var res = new List<List<string>>();
+
+                                                        foreach(var row in rows)
+                                                        {
+                                                            var cells = row.SelectNodes(".//td");
+                                                            if(cells != null && cells.Count > 0)
+                                                            {
+                                                                res.Add(new List<string>(cells.Select(c => c.InnerText)));
+                                                            }
+                                                        }
+
+                                                        result[titleNode.InnerText] = res;
+                                                    }
+                                                }
+                                            }
+                                        }
+
+                                        return result;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("No tables");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+
+            return null;
+        }
+    }
+}
---
+++ SimpleParsing/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+
+namespace SimpleParsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+
+            Dictionary<string, List<List<string>>> result = Parsing("https://www.yr.no/place/Sweden/Stockholm/Stockholm/");
+            if(result != null && result.Count > 0)
+            {
+                foreach(var r in result)
+                {
+                    Console.WriteLine(r.Key);
+
+                    foreach(var row in r.Value)
+                    {
+                        Console.WriteLine(string.Join("\t", row));
+                    }
+                }
+            }
+
+            System.Diagnostics.Debugger.Break();
+        }
+
+        private static Dictionary<string, List<List<string>>> Parsing(string url)
+        {
+            try
+            {
+                using (HttpClientHandler hdl = new HttpClientHandler { AllowAutoRedirect = false, AutomaticDecompression = System.Net.DecompressionMethods.Deflate | System.Net.DecompressionMethods.GZip })
+                {
+                    using (var client = new HttpClient(hdl))
+                    {
+                        using (HttpResponseMessage resp = client.GetAsync(url).Result)
+                        {
+                            if(resp.IsSuccessStatusCode)
+                            {
+                                var html = resp.Content.ReadAsStringAsync().Result;
+                                var doc = new HtmlAgilityPack.HtmlDocument();
+                                doc.LoadHtml(html);
+
+                                var tables = doc.DocumentNode.SelectNodes(".//div[@clas='yr-content-stickynav-three-fifths left lp_cleft']//table");
+
+                                if (tables != null && tables.Count > 0)
+                                {
+                                    Dictionary<string, List<List<string>>> result = new Dictionary<string, List<List<string>>>();
+
+                                    foreach (var table in tables)
+                                    {
+                                        var caption = table.SelectSingleNode(".//caption");
+                                        if(caption != null)
+                                        {
+                                            var rows = table.SelectNodes(".//tr");
+                                            if (rows != null)
+                                            {
+                                                var resRows = new List<List<string>>();
+                                                foreach (var row in rows)
+                                                {
+                                                    var cells = row.SelectNodes(".//td");
+                                                    if (cells != null)
+                                                    {
+                                                        resRows.Add(new List<string>(cells.Select(c => c.InnerText)));
+                                                    }
+                                                }
+
+                                                result[caption.InnerText] = resRows;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+
+            return null;
+        }
+    }
+}
