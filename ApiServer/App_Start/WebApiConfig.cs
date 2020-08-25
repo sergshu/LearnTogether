01580b6eeac0c8d3a7be78d884cdc0aa421a61d2
Sergey Shushert <sergeyshushert@outlook.com>
2020-08-25 14:43:58 +0300

    Added prj ApiServer.

---
+++ ApiServer/App_Start/WebApiConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace ApiServer
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("application/javascript"));
+        }
+    }
+}
---
+++ ApiServer/Controllers/DefaultController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ApiServer.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        [AcceptVerbs("GET")]
+        [Route("Ping/{id}")]
+        public async Task<IHttpActionResult> Ping(int id)
+        {
+            return Ok(new { date = DateTime.Now, data = id });
+        }
+
+        [AcceptVerbs("POST")]
+        [Route("PostData")]
+        public async Task<IHttpActionResult> PostData([FromBody] string[] data)
+        {
+            return Ok(new { date = DateTime.Now, data });
+        }
+    }
+}
