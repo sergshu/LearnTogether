6bee57f7b2ed92ddc86eb2fc3e01e16c5d227754
Sergey Shushert <sergeyshushert@outlook.com>
2020-08-25 14:46:26 +0300

    Added POST data for method PostData

--- ApiServer/Controllers/DefaultController.cs
+++ ApiServer/Controllers/DefaultController.cs
@@ -17,6 +17,18 @@ namespace ApiServer.Controllers
             return Ok(new { date = DateTime.Now, data = id });
         }
 
+        /// <summary>
+        /// Передаваемые данные:
+        //POST https://localhost:44318/PostData HTTP/1.1
+        //Content-Type: application/javascript
+        //Cache-Control: no-cache
+        //Host: localhost:44318
+        //Content-Length: 19
+
+        //["first", "second"]
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
         [AcceptVerbs("POST")]
         [Route("PostData")]
         public async Task<IHttpActionResult> PostData([FromBody] string[] data)
