e92cceaebdb6533bcda18bd9670331c9ea51cbfe
Sergey Shushert <sergeyshushert@outlook.com>
2020-10-11 14:54:53 +0300

    Added WindowsService prj

---
+++ WindowsService1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            if (Environment.UserInteractive)
+            {
+                if(args != null && args.Length > 0)
+                {
+                    switch(args[0])
+                    {
+                        case "--install":
+                            try
+                            {
+                                var appPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+                                System.Configuration.Install.ManagedInstallerClass.InstallHelper(new string[] { appPath });
+                            }
+                            catch(Exception ex) { Console.WriteLine(ex.Message); }
+                            break;
+                        case "--uninstall":
+                            try
+                            {
+                                var appPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+                                System.Configuration.Install.ManagedInstallerClass.InstallHelper(new string[] {"/u", appPath });
+                            }
+                            catch(Exception ex) { Console.WriteLine(ex.Message); }
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[]
+                {
+                new Service1()
+                };
+                ServiceBase.Run(ServicesToRun);
+            }
+        }
+    }
+}
---
+++ WindowsService1/Service1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace WindowsService1
+{
+    public partial class Service1 : ServiceBase
+    {
+        private bool _cancel;
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _cancel = false;
+
+            Task.Run(() => Processing());
+
+            EventLog.WriteEntry("We did it! Started", EventLogEntryType.Information);
+        }
+
+        private void Processing()
+        {
+            try
+            {
+                while(!_cancel)
+                {
+                    Thread.Sleep(100);
+                }
+            }
+            catch(Exception ex) { Console.WriteLine(ex.Message); }
+        }
+
+        protected override void OnStop()
+        {
+            _cancel = true;
+
+            EventLog.WriteEntry("We did it! Stoped", EventLogEntryType.Information);
+        }
+    }
+}
