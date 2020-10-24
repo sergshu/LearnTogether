96765b4805637aa062a487684f57635131f298c3
Sergey Shushert <sergeyshushert@outlook.com>
2020-10-24 13:12:17 +0300

    Added prj ExcelTest

---
+++ ExcelTest/BO/ExcelHelper.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelTest.BO
+{
+    class ExcelHelper : IDisposable
+    {
+        private Excel.Application _excel;
+        private Excel.Workbook _workbook;
+        private string _filePath;
+
+        public ExcelHelper()
+        {
+            _excel = new Excel.Application();
+        }
+
+        internal bool Open(string filePath)
+        {
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    _workbook = _excel.Workbooks.Open(filePath);
+                }
+                else
+                {
+                    _workbook = _excel.Workbooks.Add();
+                    _filePath = filePath;
+                }
+
+                return true;
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+            return false;
+        }
+
+        internal void Save()
+        {
+            if (!string.IsNullOrEmpty(_filePath))
+            {
+                _workbook.SaveAs(_filePath);
+                _filePath = null;
+            }
+            else
+            {
+                _workbook.Save();
+            }
+        }
+
+        internal bool Set(string column, int row, object data)
+        {
+            try
+            {
+                ((Excel.Worksheet)_excel.ActiveSheet).Cells[row, column] = data;
+                return true;
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+            return false;
+        }
+
+        public void Dispose()
+        {
+            try
+            {
+                _workbook.Close();
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+    }
+}
---
+++ ExcelTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace ExcelTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (BO.ExcelHelper helper = new BO.ExcelHelper())
+                {
+                    if (helper.Open(filePath: Path.Combine(Environment.CurrentDirectory, "TestLT.xlsx")))
+                    {
+                        helper.Set(column: "A", row: 1, data: "lksadklsajdkl");
+                        helper.Set(column: "B", row: 1, data: DateTime.Now);
+
+                        helper.Save();
+                    }
+                }
+
+                Console.Read();
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+    }
+}
