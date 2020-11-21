20a73d57045b02740188bc57d31e85d818820282
Sergey Shushert <sergeyshushert@outlook.com>
2020-11-21 16:34:00 +0200

    Added WpfApp1

---
+++ WpfApp1/Controlers/MainControler.cs
@@ -0,0 +1,38 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.ViewModel;
+
+namespace WpfApp1.Controlers
+{
+    class MainControler
+    {
+        private MainViewModel _model;
+
+        public MainControler()
+        {
+            _model = CommonServiceLocator.ServiceLocator.Current.GetInstance<MainViewModel>();
+
+            _model.StartCommand = new RelayCommand(Start);
+
+            _model.PropertyChanged += _model_PropertyChanged;
+        }
+
+        private void _model_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case "Text": break;
+                case "ResultText": _model.Text = _model.ResultText; break;
+            }
+        }
+
+        private void Start()
+        {
+            _model.ResultText = _model.Text;
+        }
+    }
+}
---
+++ WpfApp1/ViewModel/MainViewModel.cs
@@ -0,0 +1,44 @@
+using GalaSoft.MvvmLight;
+using System.Windows.Input;
+
+namespace WpfApp1.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+    /// </para>
+    /// <para>
+    /// You can also use Blend to data bind with the tool's support.
+    /// </para>
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        private string text;
+        private string resultText;
+
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel()
+        {
+            ////if (IsInDesignMode)
+            ////{
+            ////    // Code runs in Blend --> create design time data.
+            ////}
+            ////else
+            ////{
+            ////    // Code runs "for real"
+            ////}
+        }
+
+        public string Text { get => text; set { text = value; RaisePropertyChanged(nameof(Text)); } }
+
+        public string ResultText { get => resultText; set { resultText = value; RaisePropertyChanged(nameof(ResultText)); } }
+
+        public ICommand StartCommand { get; set; }
+    }
+}
