017e5c021bc6c25e2f70a6afa1eceb51473e2447
Sergey Shushert <sergeyshushert@outlook.com>
2020-11-22 11:32:46 +0200

    Added WebApp MVC Dot.Net

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Test()
+        {
+            return View(new Models.TestModel());
+        }
+
+        [HttpPost]
+        public ActionResult Test(Models.TestModel model)
+        {
+            model.Result = $"You entered text `{model.Text}`. Selected `{model.Selected}`";
+            model.MyItems = new List<Models.MyItemModel> { new Models.MyItemModel { Id = 1, Name = "First", UserName = "User1" }, new Models.MyItemModel { Id = 2, Name = "Second", UserName = "User11" }, };
+            return View(model);
+        }
+
+        public ActionResult TestItemView(int id)
+        {
+            return View(new Models.MyItemModel { Id = id, Name = "Model" + id, UserName = "User" + id });
+        }
+
+    }
+}
---
+++ WebApplication1/Models/TestModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class TestModel
+    {
+        public string Text { get; set; } = "Test";
+        public string Result { get; set; }
+        public string Selected { get; set; }
+        public IEnumerable<string> Items { get; set; } = new List<string> { "1", "2", "3" };
+
+        public IEnumerable<MyItemModel> MyItems { get; set; }
+    }
+
+    public class MyItemModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string UserName { get; set; }
+    }
+}
