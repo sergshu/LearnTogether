ebed0e46428bc7527ddbc0ea185bbbcc2529aa2f
Sergey Shushert <sergeyshushert@outlook.com>
2021-01-24 11:13:50 +0200

    Lesson 7

---
+++ Lesson7/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Lesson7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new TestEvent();
+            test.NewSecond += (sender, e)=> { write($"New second {e} Lambda"); } ;
+            test.NewSecond += Test_NewSecond;
+
+            Task.Run(()=> test.Start());
+
+            Console.WriteLine("Нажмите кнопку");
+            Console.ReadKey();
+
+            test.Stop();
+        }
+
+        private static void Test_NewSecond(object sender, int e)
+        {
+            write($"New second {e}");
+        }
+
+        private static void writeNewLine()
+        {
+            Console.WriteLine();
+        }
+
+        private static void write(string v)
+        {
+            Console.WriteLine(v);
+        }
+    }
+}
---
+++ Lesson7/TestEvent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lesson7
+{
+    class TestEvent
+    {
+        private bool _cancel = false;
+
+        public event EventHandler<int> NewSecond;
+
+        internal void Start()
+        {
+            int second = 0;
+            while (!_cancel)
+            {
+                second += 5;
+                Thread.Sleep(1000);
+                //NewSecond?.Invoke(this, ++second);
+                NewSecond?.Invoke(this, second);
+            }
+        }
+
+        internal void Stop()
+        {
+            _cancel = true;
+        }
+    }
+}
