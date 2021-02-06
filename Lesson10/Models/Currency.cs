a7d2452b7c00a63fba339d45af4c91495fd9b36c
Sergey Shushert <sergeyshushert@outlook.com>
2021-02-06 14:01:23 +0200

    Lesson 10

---
+++ Lesson10/Models/Currency.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson8.Models
+{
+    public class Currency
+    {
+        [Newtonsoft.Json.JsonProperty("currency_id")]
+        public CurrencyEnum CurrencyId { get; set; }
+
+        [Newtonsoft.Json.JsonProperty("date")]
+        [Newtonsoft.Json.JsonConverter(typeof(CustomDateConverter))]
+        public DateTime? Date { get; set; }
+        
+        [Newtonsoft.Json.JsonProperty("value")]
+        public decimal Value { get; set; }
+        
+        [Newtonsoft.Json.JsonProperty("ratio")]
+        public int Ratio { get; set; }
+    }
+
+}
---
+++ Lesson10/Models/CustomDateConverter.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Lesson8.Models
+{
+    internal class CustomDateConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.Value != null && reader.Value is long)
+            {
+                return new DateTime(1970, 1, 1).AddSeconds((long)reader.Value);
+            }
+
+            if (reader.Value != null && reader.Value is string)
+            {
+                var strDate = (string)reader.Value;
+                if (DateTime.TryParseExact(strDate, "yyyy-MM-dd", null, System.Globalization.DateTimeStyles.None, out DateTime dt))
+                {
+                    return dt;
+                }
+
+                if (DateTime.TryParseExact(strDate, "yyyyMMddHHmmss", null, System.Globalization.DateTimeStyles.None, out DateTime dt1))
+                {
+                    return dt1;
+                }
+            }
+
+            return null;
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return true;
+        }
+    }
+}
