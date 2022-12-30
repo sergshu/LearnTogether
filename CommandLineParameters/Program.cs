df9f99c5e45c31b99949efc3018dcbcd50cf556a
Sergey Shushert <sergeyshushert@outlook.com>
2022-12-30 20:10:47 +0200

    Command line parameters

---
+++ CommandLineParameters/Program.cs
@@ -0,0 +1,37 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using System.ComponentModel.DataAnnotations;
+
+[Command(Description ="Programm description", Name ="Test App")]
+[HelpOption("-?|--help")]
+public class Program
+{
+    [Required]
+    [Option("-n|--name", CommandOptionType.SingleValue, Description = "Name")]
+    public string Name { get; set; }
+
+    [Option("-d|--debug", CommandOptionType.SingleOrNoValue, Description = "Debug")]
+    public bool Debug { get; set; }
+
+    [Option("-p|--phone", CommandOptionType.MultipleValue, Description = "Phones")]
+    public string[] Phones { get; set; }
+
+    public static void Main(string[] args) 
+        => CommandLineApplication.Execute<Program>(args);
+
+    public void OnExecute()
+    {
+        // See https://aka.ms/new-console-template for more information
+        Console.WriteLine("Hello, World!");
+
+        Console.WriteLine("Name: " + Name);
+        Console.WriteLine("Debug: " + Debug);
+        if (Phones != null)
+        {
+            Console.WriteLine("Phones: " + string.Join(" ", Phones));
+        }
+        else
+        {
+            Console.WriteLine("Phones empty");
+        }
+    }
+}
